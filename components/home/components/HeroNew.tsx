e69dd26a4dc9da82b21cf98c798b3761572c50d4
ekin04 <mirko.chiaverini@gmail.com>
2024-10-29 19:22:48 +0100

    Seo, hero rimosso convenzioni aggiunto telefono

--- components/home/components/HeroNew.tsx
+++ components/home/components/HeroNew.tsx
@@ -4,77 +4,75 @@ import conv1 from "../../../public/img/convenzioni/conv2.jpg";
 import conv2 from "../../../public/img/convenzioni/conv3.jpg";
 import conv3 from "../../../public/img/convenzioni/conv4.jpg";
 import conv4 from "../../../public/img/convenzioni/conv8.jpg";
+import { PhoneIcon } from "@heroicons/react/solid";
 
 interface HeroNewProps {}
 
 export const HeroNew: React.FC<HeroNewProps> = () => {
   return (
     <>
-    <div className="w-full">
-    
-      <div className="container mx-auto pt-2 pb-5 lg:pt-10 lg:pb-0 px-4 sm:px-6 lg:px-8 ">
-        <div className="grid lg:grid-cols-7 lg:gap-x-8 xl:gap-x-12 lg:items-center ">
-          <div className="lg:col-span-3">
-            <div
-              className="flex justify-center items-center py-3 px-6 mr-10 lg:hidden"
-              data-aos="fade-in"
-              data-aos-duration="1500"
-              data-aos-once="true"
-            >
-              <Image
-                src={"/img/Fisioste_Logo.webp"}
-                width="65px"
-                height="60px"
-                layout="intrinsic"
-                alt="Fisioste Logo"
-              />
-              <Image
-                src={"/img/home/Fiosioste_Font.webp"}
-                width="180x"
-                height="45px"
-                layout="intrinsic"
-                alt="Fisioste Logo"
-              />
-            </div>
-            <h1
-              className="block mt-1 lg:mt-6 text-center lg:text-left text-3xl font-bold text-gray-800 sm:text-4xl lg:text-5xl"
-              data-aos="fade-right"
-              data-aos-duration="1000"
-              data-aos-once="true"
-            >
-              Fisioterapia e Osteopatia Teramo
-            </h1>
-            <p
-              className="mt-4 lg:mt-10 text-lg ml-1 text-gray-800 text-center lg:text-left xl:w-10/12"
-              data-aos="fade-right"
-              data-aos-duration="1000"
-              data-aos-once="true"
-              data-aos-delay="300"
-            >
-              Professionalità e tecnologia al servizio del benessere e del
-              recupero del paziente.
-            </p>
+      <div className="w-full">
+        <div className="container mx-auto pt-2 pb-5 lg:pt-10 lg:pb-0 px-4 sm:px-6 lg:px-8 ">
+          <div className="grid lg:grid-cols-7 lg:gap-x-8 xl:gap-x-12 lg:items-center ">
+            <div className="lg:col-span-3 lg:ml-8">
+              <div
+                className="flex justify-center items-center py-3 px-6 mr-10 lg:hidden"
+                data-aos="fade-in"
+                data-aos-duration="1500"
+                data-aos-once="true"
+              >
+                <Image
+                  src={"/img/Fisioste_Logo.webp"}
+                  width="65px"
+                  height="60px"
+                  layout="intrinsic"
+                  alt="Fisioste Logo"
+                />
+                <Image
+                  src={"/img/home/Fiosioste_Font.webp"}
+                  width="180x"
+                  height="45px"
+                  layout="intrinsic"
+                  alt="Fisioste Logo"
+                />
+              </div>
+              <h1
+                className="block mt-1 lg:mt-6 text-center lg:text-left text-3xl font-bold text-gray-800 sm:text-4xl lg:text-5xl"
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                data-aos-once="true"
+              >
+                Fisioterapia e Osteopatia Teramo
+              </h1>
+              <p
+                className="mt-4 lg:mt-10 text-lg ml-1 text-gray-800 text-center lg:text-left xl:w-10/12"
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                data-aos-once="true"
+                data-aos-delay="300"
+              >
+                Professionalità e tecnologia al servizio del benessere e del
+                recupero del paziente.
+              </p>
 
-            <div
-              className="mt-5 xl:mt-8 flex flex-col items-center gap-2 sm:flex-row sm:gap-3"
-              data-aos="fade-right"
-              data-aos-duration="1000"
-              data-aos-once="true"
-              data-aos-delay="700"
-            >
-             
-            </div>
+              <div
+                className="mt-5 xl:mt-8 flex flex-col items-center gap-2 sm:flex-row sm:gap-3"
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                data-aos-once="true"
+                data-aos-delay="700"
+              ></div>
 
-            <div
-              className=" lg:mt- text-center lg:text-left"
-              data-aos="fade-in"
-              data-aos-duration="1000"
-              data-aos-once="true"
-              data-aos-delay="1200"
-              data-aos-offset="-200"
-            >
-              <span className="text-xs font-medium text-gray-500 uppercase">
-                Convenzionato con:
+              <div
+                className=" lg:mt- text-center lg:text-left"
+                data-aos="fade-in"
+                data-aos-duration="1000"
+                data-aos-once="true"
+                data-aos-delay="800"
+                data-aos-offset="-200"
+              >
+                {/*  <span className="text-xs font-medium text-gray-500 uppercase">
+                Convenzionato con: 
               </span>
 
               <div className="gap-x-6 px-4 lg:px-0 grayscale opacity-80 items-center grid grid-cols-4 mix-blend-multiply">
@@ -104,22 +102,26 @@ export const HeroNew: React.FC<HeroNewProps> = () => {
                   width={100}
                   height={120}
                 />
+              </div> */}
+                <a href="tel:+393288349050" className="flex justify-center items-center w-1/2 text-white bg-secondary hover:bg-primary transition px-3 py-2 rounded-3xl mx-auto lg:mx-0">
+                  <PhoneIcon className="mr-5 w-8 h-8" />
+                  3288349050
+                </a>
               </div>
             </div>
-          </div>
 
-          <div
-            className="lg:col-span-4 mt-2 md:mt-10 lg:mt-0 rounded-3xl"
-            data-aos="fade-left"
-            data-aos-duration="1000"
-            data-aos-once="true"
-            data-aos-delay="300"
-          >
-            <CaroselloHeroHome />
+            <div
+              className="lg:col-span-4 mt-6 md:mt-10 lg:mt-0 rounded-3xl"
+              data-aos="fade-left"
+              data-aos-duration="1000"
+              data-aos-once="true"
+              data-aos-delay="300"
+            >
+              <CaroselloHeroHome />
+            </div>
           </div>
         </div>
-      </div>
-      <div className=" hidden  lg:flex pt-10 w-full bg-gradient-to-b from-white to-[#efefef]"></div>
+        <div className=" hidden  lg:flex pt-10 w-full bg-gradient-to-b from-white to-[#efefef]"></div>
       </div>
     </>
   );
--- components/shared/SeoImage.tsx
+++ components/shared/SeoImage.tsx
@@ -15,51 +15,73 @@ interface SeoProps {
 const Seo: React.FC<SeoProps> = ({
   metadata = {
     title: "",
-    description: "Fisioterapia e Osteopatia. Professionalità e tecnologia al servizio del benessere e del recupero del paziente.",
+    description:
+      "Fisioterapia e Osteopatia. Professionalità e tecnologia al servizio del benessere e del recupero del paziente.",
     url: "",
     index: true,
     image: "",
   },
 }) => {
-    const baseUrl = "https://fisioste.it"
+  const baseUrl = "https://fisioste.it";
   return (
     <>
       <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Fisioste" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
         <title>{"Fisioste | " + metadata.title}</title>
         <meta name="title" content={"Fisioste | " + metadata.title} />
-        <meta name="description" content={metadata.description.slice(0,150)+" ..."} />
+        <meta
+          name="description"
+          content={metadata.description.slice(0, 150) + " ..."}
+        />
         <meta
           name="keywords"
           content="Fisioterapia,Fisioste,Osteopatia,Teramo"
         />
         <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content={baseUrl +"/" + metadata.url}
-        />
+        <meta property="og:url" content={baseUrl + "/" + metadata.url} />
         <link rel="canonical" href={baseUrl + "/" + metadata.url} />
         <meta property="og:title" content={"Fisioste | " + metadata.title} />
-        <meta property="og:site_name" content="Fisioste - Fisioterapia e Osteopatia Teramo"  />
+        <meta
+          property="og:site_name"
+          content="Fisioste - Fisioterapia e Osteopatia Teramo"
+        />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:image" content={baseUrl + metadata.image} />
         <meta property="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:url"
-          content={baseUrl + "/" + metadata.url}
-        />
+        <meta property="twitter:url" content={baseUrl + "/" + metadata.url} />
         <meta
           property="twitter:title"
           content={"Fisioste | " + metadata.title}
         />
         <meta property="twitter:description" content={metadata.description} />
         <meta property="twitter:image" content={baseUrl + metadata.image} />
-        <meta name="robots" content={metadata.index ? "index, follow" : "noindex, nofollow"}/>
-        <meta name="googlebot" content={metadata.index ? "index, follow" : "noindex, nofollow"}/>
+        <meta
+          name="robots"
+          content={metadata.index ? "index, follow" : "noindex, nofollow"}
+        />
+        <meta
+          name="googlebot"
+          content={metadata.index ? "index, follow" : "noindex, nofollow"}
+        />
         <link rel="author" href={baseUrl} />
         <meta name="author" content="Fisioste" />
         <link rel="author" href="https://digital-three.com" />
         <meta name="author" content="Digital-Three" />
-     </Head>
+      </Head>
     </>
   );
 };
